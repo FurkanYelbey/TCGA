18d2a405acba73d089ee127715857b41f3c686f8
Furkan Yelbey <leona.vermillion@outlook.com>
2022-10-16 21:32:27 +0300

    Comment Section

--- MergeTsv.py
+++ MergeTsv.py
@@ -6,37 +6,30 @@ path = "E:\TCGA\\TCGA\\files" # path folder
 dirs = os.chdir(path)
 
 sample_id_file = "../files/gdc_sample_sheet.tsv"
-
 sample_id = pd.read_csv(sample_id_file, sep='\t')
-sample = sample_id['Sample ID'].tolist()
-sampleFile = sample_id['File Name'].tolist()
+sample = sample_id['Sample ID'].tolist()            # Sample IDs
+sampleFile = sample_id['File Name'].tolist()        # Sample's File Name
 
-df = pd.DataFrame()
+df = pd.DataFrame() #Creating Empty DataFrame
 
-def firstfunction():
+def MergeTsv():
     for file in os.listdir():
         # Check whether file is in text format or not
         if file.endswith("counts.tsv"):
             file_path = f"{path}\\{file}"
-            #print(file)
 
             file1 = pd.read_csv(file_path, sep='\t', skiprows=6, header=None)
 
             gene_values = file1[3].tolist()
 
             mergeIndex = 0 #merged table patient name column index
-            for sampleIndex in range(len(sample)):
-                #print(i)
-                if file == sampleFile[sampleIndex]:
+            for sampleIndex in range(len(sample)): #SampleCount
+                if file == sampleFile[sampleIndex]: #Does file match sampleFile if matches put it into right index of columns
                     df.insert(mergeIndex, sample[sampleIndex], gene_values)
                     mergeIndex += 1
 
     gene_names = file1[0]
     df.insert(0, None, gene_names)
-
-    print(df)
-    #print(df.to_string(index=False))
-
     df.to_csv('example.tsv', sep="\t", index = False)
 
-firstfunction()
+MergeTsv()
