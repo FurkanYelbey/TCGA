6b51800623f54c764aa96c49f28237ebbb27ded4
Furkan Yelbey <leona.vermillion@outlook.com>
2022-10-22 23:50:57 +0300

    File Control

--- MergeTsv.py
+++ MergeTsv.py
@@ -1,32 +1,38 @@
 import pandas as pd
 import os
 
-path = os.getcwd()+"\\files" # path folder
+path = os.getcwd()+"\\files"
 
-dirs = os.chdir(path)
-
-sample_id_file = "../files/gdc_sample_sheet.tsv"
+sample_id_file = "files/gdc_sample_sheet.2022-10-22.tsv"
 sample_id = pd.read_csv(sample_id_file, sep='\t')
 sample = sample_id['Sample ID'].tolist()            # Sample IDs
 sampleFile = sample_id['File Name'].tolist()        # Sample's File Name
 
 df = pd.DataFrame() #Creating Empty DataFrame
 
+subfolders = [f.path for f in os.scandir(path) if f.is_dir()] # named by fileID
+
 def MergeTsv():
-    for file in os.listdir():
-        # Check whether file is in text format or not
-        if file.endswith("counts.tsv"):
-            file_path = f"{path}\\{file}"
+    for paths in subfolders:
+        for file in os.listdir(paths):
+            # Check whether file is in text format or not
+            if file.endswith("counts.tsv"):
+                file_path = f"{paths}\\{file}"
 
-            file1 = pd.read_csv(file_path, sep='\t', skiprows=6, header=None)
+                file1 = pd.read_csv(file_path, sep='\t', skiprows=6, header=None)
 
-            gene_values = file1[3].tolist()
+        #        #x = len(gene_values)
+        #        #y = x%10000
 
-            mergeIndex = 0 #merged table patient name column index
-            for sampleIndex in range(len(sample)): #SampleCount
-                if file == sampleFile[sampleIndex]: #Does file match sampleFile if matches put it into right index of columns
-                    df.insert(mergeIndex, sample[sampleIndex], gene_values)
-                    mergeIndex += 1
+                gene_values = file1[3].tolist()
+
+
+                mergeIndex = 0 #merged table patient name column index
+                for sampleIndex in range(len(sample)): #SampleCount
+                    if file == sampleFile[sampleIndex]: #Does file match sampleFile if matches put it into right index of columns
+                        df.insert(mergeIndex, sample[sampleIndex], gene_values)
+                        mergeIndex += 1
+
 
     gene_names = file1[0]
     df.insert(0, None, gene_names)
